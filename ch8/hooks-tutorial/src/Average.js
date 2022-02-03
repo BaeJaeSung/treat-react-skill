a4d49176609aef62d8fa721a03b0d9e13a3a1304
anonymouspotter <anonymouspotter@protonmail.com>
2022-02-04 04:28:49 +0900

    finish ch8 and create ch10

---
+++ ch8/hooks-tutorial/src/Average.js
@@ -0,0 +1,46 @@
+import { useState, useMemo, useCallback, useRef } from "react";
+
+const getAverage = (numbers) => {
+  console.log("calc avg");
+  if (numbers.length === 0) return 0;
+  const sum = numbers.reduce((a, b) => a + b);
+  return sum / numbers.length;
+};
+
+const Average = () => {
+  const [list, setList] = useState([]);
+  const [number, setNumber] = useState("");
+  const inputEl = useRef(null);
+
+  const onChange = useCallback((e) => {
+    setNumber(e.target.value);
+  }, []);
+  const onInsert = useCallback(
+    (e) => {
+      const nextList = list.concat(parseInt(number));
+      setList(nextList);
+      setNumber("");
+      inputEl.current.focus();
+    },
+    [number, list]
+  );
+
+  const avg = useMemo(() => getAverage(list), [list]);
+
+  return (
+    <div>
+      <input value={number} onChange={onChange} ref={inputEl} />
+      <button onClick={onInsert}>insert</button>
+      <ul>
+        {list.map((value, index) => (
+          <li key={index}>{value}</li>
+        ))}
+      </ul>
+      <div>
+        <p>avg value = {avg}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Average;
