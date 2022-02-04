be7e5eb08f91bfc25bbdd869ac9fecf502e2023b
anonymouspotter <anonymouspotter@protonmail.com>
2022-02-04 18:52:03 +0900

    delete useless todo-app. it's like a trash. oh trash says "please don't compare with me." And Uploaded a new todo-app2

---
+++ ch10/todo-app2/src/components/TodoInsert.js
@@ -0,0 +1,36 @@
+import { useState, useCallback, useRef } from 'react';
+import { MdAdd } from 'react-icons/md';
+import { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } from '../../node_modules/react/cjs/react.production.min';
+import './TodoInsert.scss';
+
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue('');
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
+
+  return (
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={onChange}
+      />
+      <button type="submit">
+        <MdAdd />
+      </button>
+    </form>
+  );
+};
+
+export default TodoInsert;
