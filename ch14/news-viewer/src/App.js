5c355e66b14ab5806942c0933a89fb2f59b0d425
anonymouspotter <anonymouspotter@protonmail.com>
2022-02-04 23:19:53 +0900

    finish ch14 and created ch15

---
+++ ch14/news-viewer/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=30144dd366a0427299ea95ccf2f71a87',
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>load</button>
+      </div>
+      {data && (
+        <textarea
+          row={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
